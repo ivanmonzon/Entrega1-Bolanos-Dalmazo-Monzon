1cf4c57e46c588a20e0e11d8d0eab988b1f7c37b
ivanmonzon <ivomonzon@gmail.com>
2022-06-09 21:28:03 -0300

    Cambié las clases para que sean más descriptivas en el admin

--- App1/models.py
+++ App1/models.py
@@ -10,13 +10,17 @@ class Artist(models.Model):
 class Label(models.Model):
     name=models.CharField(max_length=40)
     country=models.CharField(max_length=40)
+    def __str__(self):
+        return f"Name: {self.name} - Country: {self.country}"
 
 class Instrument(models.Model):
     name=models.CharField(max_length=40)
     type=models.CharField(max_length=40)
-
+    def __str__(self):
+        return f"Name: {self.name} - Type: {self.type}"
 class Genre(models.Model):
     name=models.CharField(max_length=40)
     country_of_origin=models.CharField(max_length=40)
-
+    def __str__(self):
+        return f"Name: {self.name} - Country of origin: {self.country_of_origin}"
     
