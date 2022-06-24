5bb8eb4e6c64f71c3c52f5d35d152291a6b8077d
ivanmonzon <ivomonzon@gmail.com>
2022-06-24 20:37:38 -0300

    Cambié la ruta de index para que se pueda ingresar sólo con '/' (anteriormente era con 'App1/'

--- Project/urls.py
+++ Project/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import helpers
 from django.urls import path, include
+from App1 import views
 #linkeamos el url admin del proyecto con el url de la app, en este caso App1
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('App1/', include('App1.urls')),
+    path('', views.index, name="Index"),
 ]
 
 handler404 = 'App1.views.error_404_view'
