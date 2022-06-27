d17b84c9c6e60545bebbdbe8eeb5029b08f09073
ivanmonzon <ivomonzon@gmail.com>
2022-06-27 19:10:40 -0300

    Agregué la descripción/biografia

--- App1/models.py
+++ App1/models.py
@@ -3,12 +3,14 @@ from django.db import models
 class Artist(models.Model):
     name=models.CharField(max_length=40)
     nacionality=models.CharField(max_length=40)
+    biography = models.TextField()
     def __str__(self):
         return f"Name: {self.name} - Nacionality: {self.nacionality}"
 
 class Label(models.Model):
     name=models.CharField(max_length=40)
     country=models.CharField(max_length=40)
+    description= models.TextField()
     def __str__(self):
         return f"Name: {self.name} - Country: {self.country}"
 
@@ -16,11 +18,13 @@ class Label(models.Model):
 class Instrument(models.Model):
     name=models.CharField(max_length=40)
     type=models.CharField(max_length=40)
+    description= models.TextField()
     def __str__(self):
         return f"Name: {self.name} - Type: {self.type}"
 class Genre(models.Model):
     name=models.CharField(max_length=40)
     country_of_origin=models.CharField(max_length=40)
+    description= models.TextField()
     def __str__(self):
         return f"Name: {self.name} - Country of origin: {self.country_of_origin}"
     
